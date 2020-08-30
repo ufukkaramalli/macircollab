7ec4534278365c9f64cbfcfe5262fa73a74baaff
ufukkaramalli <ufukkaramalli@gmail.com>
2020-08-30 18:25:23 +0300

    scss

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 /* eslint-disable */
 <template>
   <div id="app">
-    <div class="container">
-      TEST PAGE
-    </div>
+    <header>
+      <div class="container">
+        HEADER
+      </div>
+    </header>
+    <main role="main">
+      <div class="container">
+        <div class="row">
+          <div
+            class="col"
+            v-for="number in 1000"
+            :key="number"
+            :style="{
+              width: randomInt(10, 100) + 'px',
+              height: randomInt(10, 100) + 'px'
+            }"
+          >
+            <span>{{ number }}</span>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 <script>
 export default {
   name: "App",
-  components: {}
+  components: {},
+  methods: {
+    randomInt: function(min, max) {
+      return min + Math.floor((max - min) * Math.random());
+    }
+  },
+  computed: {
+    rand() {
+      return this.randomNumber();
+    }
+  }
 };
 </script>
 <style lang="scss">
